60513d3b4aa7295c440e0cfe50b7d7c14b29806c
Willian-1921 <126629947+Willian-1921@users.noreply.github.com>
2023-11-18 15:09:46 -0300

    Update dados.php

--- Projeto/public/dados.php
+++ Projeto/public/dados.php
@@ -33,19 +33,14 @@ $result = mysqli_query($conn, $sql);
     $stmt = $conn->prepare($sql);
 
     if ($stmt->execute()) {
-
-		echo '
-            <div style="max-width:320px; padding:50px; border:3px solid #007cff; color:#007cff; font-family: tahoma; background:#E9F8FB; text-align:center; font-weight:800; margin:180px auto;">
-                <p style="font-size:1.3em;">' . $_POST["nome"] . ', Obrigado!</p> 
-                <p style="font-size:1.1em;">Agendamento enviado com sucesso!</p>
-            </div>';
-        echo '<meta HTTP-EQUIV="Refresh" CONTENT="3;URL=index.php">';
+        
+        header("Location: obrigado.php");
+        exit;
     } else {
         echo "Erro ao inserir no banco de dados: " . $stmt->error;
     }
-
+	
     $stmt->close();
     $conn->close();
-
 ?>
    
