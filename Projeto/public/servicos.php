bff9344b3b971e34ce56f8b122b833e2064c5848
Viniciu Cristian <git.viniciu@gmail.com>
2023-11-19 11:45:33 -0300

    atualizacoes na pagina de servicos e estilo

--- Projeto/public/servicos.php
+++ Projeto/public/servicos.php
@@ -16,28 +16,34 @@ require_once 'header.php'
 </main>
 <section class="container-fluid">
     <div class="row justify-content-around align-items-center">
+
         <div class="col-3">
-            <div class="card">
-                <img class="card-img-top" src="./assets/images/fisioterapeuta.png" alt="fisioterapeuta">
+            <div class="row">
+                <img class="card-img-top profissionais-img" src="./assets/images/fisioterapeuta.png" alt="fisioterapeuta">
                 <div class="card-body">
-                    <h3>Fisioterapeuta</h3>
+                    <h2>Fisioterapeuta</h2>
                     <p class="card-text conteudo">Nossos fisioterapeutas são especializados em melhorar a qualidade de vida dos idosos. Com exercícios e técnicas personalizadas, eles promovem mobilidade, força e bem-estar.</p>
                 </div>
             </div>
         </div>
+
         <div class="col-3">
             <div class="row">
                 <img class="profissionais-img" src="./assets/images/cuidador.jpg" alt="fisioterapeuta">
-                <h2>Cuidador</h2>
-                <p class="conteudo">Nossos cuidadores são dedicados a oferecer cuidados personalizados e atenção cuidadosa para a terceira idade. Sua empatia e compromisso garantem um tratamento especializado, proporcionando apoio essencial.
-                </p>
+                <div class="card-body">
+                    <h2>Cuidador</h2>
+                    <p class="conteudo">Nossos cuidadores são dedicados a oferecer cuidados personalizados e atenção cuidadosa para a terceira idade. Sua empatia e compromisso garantem um tratamento especializado, proporcionando apoio essencial.</p>
+                </div>
             </div>
         </div>
+
         <div class="col-3">
             <div class="row">
                 <img class="profissionais-img" src="./assets/images/geriatra.png" alt="fisioterapeuta">
-                <h2>Geriatra</h2>
-                <p class="conteudo">Nossos médicos trazem a consulta até a casa do paciente. Com experiência e compreensão das necessidades dos idosos, eles oferecem cuidados médicos de qualidade, garantindo conforto e conveniência.</p>
+                <div class="card-body">
+                    <h2>Geriatra</h2>
+                    <p class="conteudo">Nossos médicos trazem a consulta até a casa do paciente. Com experiência e compreensão das necessidades dos idosos, eles oferecem cuidados médicos de qualidade, garantindo conforto e conveniência.</p>
+                </div>
             </div>
         </div>
 
