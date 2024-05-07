dfcac29f9ca5e689d7c626c940d3fe86e2cdce3a
Willian Zuqui <wpszuqui@gmail.com>
2024-05-07 11:59:29 -0300

    Modifcação de var para WAMP sem Docker, e adição de Controllers/Models/Migrations para tabelas Clientes/Especialidades/Medicos/Agendamentos

--- projeto-laravel/idosix/database/migrations/0001_01_01_000000_create_users_table.php
+++ projeto-laravel/idosix/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('tipo');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
