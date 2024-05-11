67ce7dad4bacd21248cc5e48c18f63704e1a5148
vinicius.oliveira <vinicius.git14@gmail.com>
2024-05-11 18:42:04 -0300

    .

--- projeto-laravel/idosix/routes/web.php
+++ projeto-laravel/idosix/routes/web.php
@@ -18,7 +18,8 @@ Route::get('/', function () {
 Route::get('/agendamento', 'App\Http\Controllers\AgendamentoController@abrir')->name('agendamento');
 Route::get('/inicial', 'App\Http\Controllers\InicialController@abrir')->name('inicial');
 Route::get('/servicos', 'App\Http\Controllers\ServicosController@abrir')->name('servicos');
-Route::get('/cliente/dashboard',[App\Http\Controllers\ClienteController::class, 'index'])->name('dashboard.cliente')->middleware(['auth', 'tipo:cliente']);
+Route::get('
+',[App\Http\Controllers\ClienteController::class, 'index'])->name('dashboard.cliente')->middleware(['auth', 'tipo:cliente']);
 Route::get('/admin/dashboard',[App\Http\Controllers\AdminController::class, 'index'])->name('dashboard.admin')->middleware(['auth', 'tipo:admin']);
 
 
